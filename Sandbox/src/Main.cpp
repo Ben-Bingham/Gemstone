d702945ef8198a515479c909c4c70669e3d343c6
Ben-Bingham <ben@benbingham.ca>
2022-09-07 17:18:00 -0600

    Removed unnecessary testing code

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -5,13 +5,5 @@
 int main() {
 	std::cout << "Hello World!" << std::endl;
 
-	Malachite::Vector2f vec1{ 0, 0 };
-
-	Malachite::Vector2f vec2{ 5, 4 };
-
-	vec1 += vec2;
-
-	std::cout << vec1 << vec2;
-
 	std::cin.get();
 }
