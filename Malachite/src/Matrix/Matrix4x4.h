b03dc06e45f4f61c2967fd5e1876ca282573f594
Ben-Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2022-09-08 09:54:46 -0600

    Matrixx4x4 continued

--- Malachite/src/Matrix/Matrix4x4.h
+++ Malachite/src/Matrix/Matrix4x4.h
@@ -1,9 +1,22 @@
 #pragma once
+#include <initializer_list>
 
-template<typename T>
-class Matrix4x4 {
-public:
-	Matrix4x4() {}
+#include "vector/Vector.h"
 
+namespace Malachite {
+	template<typename T>
+	class Matrix4x4 {
+	public:
+		Matrix4x4() {}
+		Matrix4x4(T val) : row1(val), row2(val), row3(val), row4(val) {}
+		Matrix4x4(Vector4<T> Row1, Vector4<T> Row2, Vector4<T> Row3, Vector4<T> Row4) : row1(Row1), row2(Row2), row3(Row3), row4(Row4) {}
+		Matrix4x4(std::initializer_list<T> initList) {
+			if (initList.size())
+		}
 
-};
+		Vector4<T> row1{ };
+		Vector4<T> row2{ };
+		Vector4<T> row3{ };
+		Vector4<T> row4{ };
+	};
+}
