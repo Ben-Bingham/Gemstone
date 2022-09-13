9fb3758bf4e1bd4e772ceaebf09638dbc74e6441
Ben-Bingham <ben@benbingham.ca>
2022-09-13 16:22:29 -0600

    Removed some test files

--- Ruby/src/test.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
-void makeWindow() {
-	GLFWwindow* window;
-	window = glfwCreateWindow(480, 640, "Window", glfwGetPrimaryMonitor(), NULL);
-}
