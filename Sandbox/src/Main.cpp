00b3ebb956fd0d62094fe0ef3eec640b23485905
Ben-Bingham <ben@benbingham.ca>
2022-09-13 18:05:42 -0600

    layed out the basic look of the Ruby API

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
+
 #include "Window.h"
 #include "Log.h"
 
 int main() {
 	Ruby::Window window{ };
 
-	while (true) {
+	Ruby::CubeRenderable cube{/*position, width, height, depth*/};
+
+	Ruby::Renderer renderer{/*maybe window?*/};
+
+	while (window.isOpen()) {
 		window.pollEvents();
+
+		{ // Rendering
+			renderer.prep();
+
+			{ // Lighting
+
+				renderer.prepLightingPass();
+
+				renderer.drawLightingPass(cube);
+
+				renderer.finishLightingPass();
+			}
+			
+			{ // Normal Rendering
+				renderer.prepNormalPass();
+
+				renderer.drawNormalPass(cube);
+
+				renderer.finishNormalPass();
+			}
+
+			renderer.finish();
+		}
 
 		window.swapBuffers();
 	}
