bcfcda147c1f9386abb6e6ff41fdf020205f8122
Ben-Bingham <ben@benbingham.ca>
2022-09-14 16:17:32 -0600

    Shader class fully implemented

--- Ruby/src/Shaders/Shader.h
+++ Ruby/src/Shaders/Shader.h
@@ -5,13 +5,16 @@
 namespace Ruby {
 	class Shader {
 	public:
-		Shader(unsigned int shaderID, TextFile sourceFile) : m_Shader(shaderID) {}
+		Shader(unsigned int shaderID, TextFile sourceFile) : m_Shader(shaderID), m_ShaderSourceFile(sourceFile) {}
 
 		virtual void dispose() = 0;
 
 		unsigned int getShader() const { return m_Shader; };
 
+		TextFile getShaderSourceFile() const { return m_ShaderSourceFile; }
+
 	private:
 		unsigned int m_Shader;
+		TextFile m_ShaderSourceFile;
 	};
 }
