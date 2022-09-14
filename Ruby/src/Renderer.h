0947603e2e9fa6fa8c08b97a610261fb80db7972
Ben-Bingham <ben@benbingham.ca>
2022-09-14 16:17:53 -0600

    Moved shader source code to external files

--- Ruby/src/Renderer.h
+++ Ruby/src/Renderer.h
@@ -12,19 +12,6 @@ namespace Ruby {
                 LOG("GLEW failed to initialize.", Lazuli::LogLevel::TERMINAL);
             }
 
-            const char* vertexShaderSource = "#version 330 core\n"
-                "layout (location = 0) in vec3 aPos;\n"
-                "void main()\n"
-                "{\n"
-                "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                "}\0";
-            const char* fragmentShaderSource = "#version 330 core\n"
-                "out vec4 FragColor;\n"
-                "void main()\n"
-                "{\n"
-                "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                "}\n\0";
-
             unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
             glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
             glCompileShader(vertexShader);
