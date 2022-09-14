4249e2603d4cdf5a0f23c65e12cecb1db1b049a2
Ben-Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2022-09-14 12:05:57 -0600

    Created TextFile class

---
+++ Ruby/src/TextFile.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class TextFile {
+public:
+	TextFile(const std::string& path);
+	TextFile(const std::string& path, const std::string& content);
+
+	std::string getContent() { return m_Content; }
+	std::string getPath() { return m_Path; }
+private:
+	std::string m_Path;
+	std::string m_Content;
+};
+
