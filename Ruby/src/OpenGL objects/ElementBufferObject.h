9bf9517c77ef6aaf6d9eb7ec585bc970d75c9279
Ben-Bingham <ben@benbingham.ca>
2022-09-17 14:58:10 -0600

    Created EBO class

--- Ruby/src/OpenGL objects/ElementBufferObject.h
+++ Ruby/src/OpenGL objects/ElementBufferObject.h
@@ -1,1 +1,23 @@
 #pragma once
+#include <GL/glew.h>
+#include <vector>
+
+namespace Ruby {
+	class ElementBufferObject {
+	public:
+		ElementBufferObject(std::vector<unsigned int> indices) {
+			glGenBuffers(1, &m_EBO);
+
+			bind();
+
+			glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(indices[0]), &indices[0], GL_STATIC_DRAW);
+		}
+
+		void bind() const { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO); }
+		static void unbind() { glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0); }
+		void dispose() { glDeleteBuffers(1, &m_EBO); }
+
+	private:
+		unsigned int m_EBO;
+	};
+}
