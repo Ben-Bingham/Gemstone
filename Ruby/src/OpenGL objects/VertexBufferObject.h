fad59e77333ae1d706acedce18a131f2eb07e7c0
Ben-Bingham <ben@benbingham.ca>
2022-09-17 14:58:04 -0600

    Created VBO class

--- Ruby/src/OpenGL objects/VertexBufferObject.h
+++ Ruby/src/OpenGL objects/VertexBufferObject.h
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <GL/glew.h>
+#include <vector>
+
+namespace Ruby {
+	class VertexBufferObject {
+	public:
+		VertexBufferObject(const std::vector<float>& vertices) {
+			glGenBuffers(1, &m_VBO);
+
+			bind();
+
+			glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vertices[0]), &vertices[0], GL_STATIC_DRAW);
+		}
+
+		void bind() const { glBindBuffer(GL_ARRAY_BUFFER, m_VBO); }
+		static void unbind() { glBindBuffer(GL_ARRAY_BUFFER, 0); }
+		void dispose() { glDeleteBuffers(1, &m_VBO); }
+
+	private:
+		unsigned int m_VBO;
+	};
+}
