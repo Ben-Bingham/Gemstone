dcf91e67a3869f0402e21bd30c40e87e811c6290
Ben-Bingham <ben@benbingham.ca>
2022-09-17 14:58:28 -0600

    Implemented new Rendering object classes

--- Ruby/src/Renderer.h
+++ Ruby/src/Renderer.h
@@ -7,54 +7,45 @@
 #include "Shaders/FragmentShader.h"
 #include "Shaders/ShaderProgram.h"
 
+#include "OpenGL objects/VertexAttributeObject.h"
+#include "OpenGL objects/VertexBufferObject.h"
+#include "OpenGL objects/ElementBufferObject.h"
+
 namespace Ruby {
 	class Renderer {
 	public:
 		Renderer() : shaderProgram(initProgram()) {
-            float vertices[] = {
-                 0.5f,  0.5f, 0.0f,  // top right
-                 0.5f, -0.5f, 0.0f,  // bottom right
-                -0.5f, -0.5f, 0.0f,  // bottom left
-                -0.5f,  0.5f, 0.0f   // top left 
+            std::vector<float> verticies{
+                 0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f, // top right
+                 0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f, // bottom right
+                -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 0.0f, // bottom left
+                -0.5f,  0.5f, 0.0f,  0.0f, 0.0f, 0.0f  // top left 
             };
-            unsigned int indices[] = {
+
+            std::vector<unsigned int> indices{
                 0, 1, 3,  // first Triangle
                 1, 2, 3   // second Triangle
             };
-            
-            glGenVertexArrays(1, &VAO);
-            glGenBuffers(1, &VBO);
-            glGenBuffers(1, &EBO);
-            // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
-            glBindVertexArray(VAO);
 
-            glBindBuffer(GL_ARRAY_BUFFER, VBO);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+            VAO.bind();
 
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+            VertexBufferObject VBO{ verticies };
+            ElementBufferObject EBO{ indices };
 
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-            glEnableVertexAttribArray(0);
+            VBO.bind();
 
-            // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
+            EBO.bind();
 
-            // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
-            //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-
-            // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-            // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-            glBindVertexArray(0);
+            VAO.enableAttributePointer(3, AttributeType::FLOAT, 6 * sizeof(float));
+            VAO.enableAttributePointer(3, AttributeType::FLOAT, 6 * sizeof(float));
 		}
 
 		void render() {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            // draw our first triangle
             shaderProgram.use();
-            glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+            VAO.bind();
             
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		}
@@ -67,6 +58,6 @@ namespace Ruby {
         }
 
         ShaderProgram shaderProgram;
-        unsigned int VBO, VAO, EBO;
+        VertexAttributeObject VAO;
 	};
 }
