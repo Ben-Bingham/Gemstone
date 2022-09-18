b24389d590aad5a78b794329ea29857d718bd987
Ben-Bingham <ben@benbingham.ca>
2022-09-18 16:04:14 -0600

    Moved shaders into Renderer class

--- Ruby/src/Renderer.h
+++ Ruby/src/Renderer.h
@@ -9,11 +9,16 @@
 namespace Ruby {
 	class Renderer {
 	public:
-		Renderer() {}
+		Renderer() 
+            : normalShader(shaderInit("..\\Ruby\\assets\\shaders\\Default.vert", "..\\Ruby\\assets\\shaders\\Default.frag", std::vector<Ruby::Attribute>{ 3, 3 })),
+              solidShader(shaderInit("..\\Ruby\\assets\\shaders\\Solid.vert", "..\\Ruby\\assets\\shaders\\Solid.frag", std::vector<Ruby::Attribute>{ 3, 3, 2 }))
+        {
+            glEnable(GL_DEPTH_TEST);
+        }
 
         void prep() {
             glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         }
 
         void render(RenderableObject renderable) {
@@ -22,6 +27,19 @@ namespace Ruby {
 
         void finish() {
 
+        }
+
+        void dispose() {
+            normalShader.dispose();
+            solidShader.dispose();
+        }
+
+        ShaderProgram normalShader; // once there are many more shaders they can be moved into a shader Library class
+        ShaderProgram solidShader; // once there are many more shaders they can be moved into a shader Library class
+
+    private:
+        ShaderProgram shaderInit(const std::string& vertexShaderPath, const std::string& fragmentShaderPath, const std::vector<Attribute>& attributes) {
+            return ShaderProgram{ VertexShader{vertexShaderPath}, FragmentShader{fragmentShaderPath}, attributes };
         }
 	};
 }
