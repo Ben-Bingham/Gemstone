5ab6e7d672007fe69ddc0a917bcdd67f1c80a876
Ben-Bingham <ben@benbingham.ca>
2022-09-18 20:13:24 -0600

    added new shaders to renderer

--- Ruby/src/Renderer.h
+++ Ruby/src/Renderer.h
@@ -10,8 +10,9 @@ namespace Ruby {
 	class Renderer {
 	public:
 		Renderer() 
-            : normalShader(shaderInit("..\\Ruby\\assets\\shaders\\Default.vert", "..\\Ruby\\assets\\shaders\\Default.frag", std::vector<Ruby::Attribute>{ 3, 3 })),
-              solidShader(shaderInit("..\\Ruby\\assets\\shaders\\Solid.vert", "..\\Ruby\\assets\\shaders\\Solid.frag", std::vector<Ruby::Attribute>{ 3, 3, 2 }))
+            : phongShader(shaderInit("..\\Ruby\\assets\\shaders\\Phong.vert", "..\\Ruby\\assets\\shaders\\Phong.frag", std::vector<Ruby::Attribute>{ 3, 3, 2 })),
+              solidShader(shaderInit("..\\Ruby\\assets\\shaders\\Solid.vert", "..\\Ruby\\assets\\shaders\\Solid.frag", std::vector<Ruby::Attribute>{ 3, 3, 2 })),
+              testShader(shaderInit("..\\Ruby\\assets\\shaders\\test.vert", "..\\Ruby\\assets\\shaders\\test.frag", std::vector<Ruby::Attribute>{ 3, 3, 2 }))
         {
             glEnable(GL_DEPTH_TEST);
         }
@@ -30,12 +31,14 @@ namespace Ruby {
         }
 
         void dispose() {
-            normalShader.dispose();
+            phongShader.dispose();
             solidShader.dispose();
+            testShader.dispose();
         }
 
-        ShaderProgram normalShader; // once there are many more shaders they can be moved into a shader Library class
+        ShaderProgram phongShader; // once there are many more shaders they can be moved into a shader Library class
         ShaderProgram solidShader; // once there are many more shaders they can be moved into a shader Library class
+        ShaderProgram testShader;
 
     private:
         ShaderProgram shaderInit(const std::string& vertexShaderPath, const std::string& fragmentShaderPath, const std::vector<Attribute>& attributes) {
