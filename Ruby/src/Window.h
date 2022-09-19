ed471d77155432580ebc07db8aaae57e7c4f0135
Ben-Bingham <ben@benbingham.ca>
2022-09-18 20:00:51 -0600

    Added input callbacks

--- Ruby/src/Window.h
+++ Ruby/src/Window.h
@@ -15,6 +15,13 @@ namespace Ruby {
 		void swapBuffers() { glfwSwapBuffers(m_Window); }
 		void dispose();
 
+		void disableCursor() { glfwSetInputMode(m_Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); }
+		void close() { glfwSetWindowShouldClose(m_Window, true); }
+
+		// Callbacks
+		void setKeyCallback(void (*callback)(GLFWwindow* window, int key, int scancode, int action, int mods)) { glfwSetKeyCallback(m_Window, callback); }
+		void setMousePositionCallback(void (*callback)(GLFWwindow* window, double xpos, double ypos)) { glfwSetCursorPosCallback(m_Window, callback); }
+
 	private:
 		GLFWwindow* m_Window;
 	};
