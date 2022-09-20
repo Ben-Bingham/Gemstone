593211a98ad4304c165de527d7e8b1e6b0869056
Ben-Bingham <ben@benbingham.ca>
2022-09-20 16:20:02 -0600

    Created a pointLight struct

---
+++ Ruby/src/Lights.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Vector.h"
+
+namespace Ruby {
+	struct PointLight {
+		Malachite::Vector3f position{ 0.0f };
+
+		Malachite::Vector3f ambient{ 0.2f };
+		Malachite::Vector3f diffuse{ 0.5f };
+		Malachite::Vector3f specular{ 1.0f };
+	};
+}
