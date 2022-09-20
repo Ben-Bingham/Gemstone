9f862b38001d890a54c2e784a5863b9785d9d8d3
Ben-Bingham <ben@benbingham.ca>
2022-09-19 21:16:24 -0600

    Added inline

--- Malachite/src/Angles.h
+++ Malachite/src/Angles.h
@@ -6,11 +6,11 @@ namespace Malachite {
 	using Degree = float;
 	using Radian = float;
 
-	Radian degreesToRadians(Degree degree) {
+	inline Radian degreesToRadians(Degree degree) {
 		return degree * (pi / 180.0f);
 	}
 
-	Degree radiansToDegrees(Radian radian) {
+	inline Degree radiansToDegrees(Radian radian) {
 		return radian * (180.0f / pi);
 	}
 }
