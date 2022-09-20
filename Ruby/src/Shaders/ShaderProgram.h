4d876ae1c740a806344c6991885df1d56ae7eace
Ben-Bingham <ben@benbingham.ca>
2022-09-20 16:19:38 -0600

    Changed names of shader uniforms to all have the same name

--- Ruby/src/Shaders/ShaderProgram.h
+++ Ruby/src/Shaders/ShaderProgram.h
@@ -7,6 +7,7 @@
 #include "FragmentShader.h"
 #include "Vector.h"
 #include "Matrix.h"
+#include "Lights.h"
 
 #include "OpenGL objects/VertexAttributeObject.h"
 
@@ -23,23 +24,23 @@ namespace Ruby {
 		}
 
 		// Basic uniforms
-		void setInt(const std::string& variableName, const int value) const {
+		void upload(const std::string& variableName, const int value) const {
 			glUniform1i(glGetUniformLocation(m_Program, variableName.c_str()), value);
 		}
 
-		void setFloat(const std::string& variableName, const float value) const {
+		void upload(const std::string& variableName, const float value) const {
 			glUniform1f(glGetUniformLocation(m_Program, variableName.c_str()), value);
 		}
 
-		void setMatrix4f(const std::string& variableName, const Malachite::Matrix4f& matrix) const {
+		void upload(const std::string& variableName, const Malachite::Matrix4f& matrix) const {
 			glUniformMatrix4fv(glGetUniformLocation(m_Program, variableName.c_str()), 1, GL_FALSE, &matrix.row1.x);
 		}
 
-		void setVector3f(const std::string& variableName, const Malachite::Vector3f& vector) const {
+		void upload(const std::string& variableName, const Malachite::Vector3f& vector) const {
 			glUniform3fv(glGetUniformLocation(m_Program, variableName.c_str()), 1, &vector.x);
 		}
 
-		void setVector4f(const std::string& variableName, const Malachite::Vector4f& vector) const {
+		void upload(const std::string& variableName, const Malachite::Vector4f& vector) const {
 			glUniform4fv(glGetUniformLocation(m_Program, variableName.c_str()), 1, &vector.x);
 		}
 
