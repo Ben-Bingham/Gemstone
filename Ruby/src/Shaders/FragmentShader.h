67c963daadcc0de47597f0e1ddc7c0a0520f1f59
Ben-Bingham <ben@benbingham.ca>
2022-09-20 17:45:25 -0600

    All shader classes now use move semantics

--- Ruby/src/Shaders/FragmentShader.h
+++ Ruby/src/Shaders/FragmentShader.h
@@ -1,14 +1,10 @@
 #pragma once
 
-#include <GL/glew.h>
-
 #include "Shader.h"
 
 namespace Ruby {
 	class FragmentShader : public Shader {
 	public:
 		FragmentShader(const TextFile& shaderSourceFile);
-
-		void dispose() override { glDeleteShader(getShader()); }
 	};
 }
--- Ruby/src/Shaders/Shader.h
+++ Ruby/src/Shaders/Shader.h
@@ -1,13 +1,28 @@
 #pragma once
 
+#include <GL/glew.h>
+
 #include "TextFile.h"
 
 namespace Ruby {
 	class Shader {
 	public:
 		Shader(unsigned int shaderID, TextFile sourceFile) : m_Shader(shaderID), m_ShaderSourceFile(sourceFile) {}
+		Shader(Shader&) = delete;
+		Shader& operator=(Shader&) = delete;
+		virtual ~Shader() { glDeleteShader(m_Shader); }
+		Shader(Shader&& other) noexcept
+			: m_Shader(std::move(other.m_Shader)),
+			  m_ShaderSourceFile(std::move(other.m_ShaderSourceFile)) {
+			other.m_Shader = 0;
+		}
 
-		virtual void dispose() = 0;
+		Shader& operator=(Shader&& other) noexcept {
+			m_Shader = std::move(other.m_Shader);
+			other.m_Shader = 0;
+
+			m_ShaderSourceFile = std::move(other.m_ShaderSourceFile);
+		}
 
 		unsigned int getShader() const { return m_Shader; };
 
