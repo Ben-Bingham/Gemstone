ac14299a9b36bb4c61746aa2a8cbd0de4405cb42
Ben-Bingham <ben@benbingham.ca>
2022-09-22 16:25:31 -0600

    Put phong renderable into Phong namespace

--- Ruby/src/Renderable Objects/Phong/PhongRenderable.h
+++ Ruby/src/Renderable Objects/Phong/PhongRenderable.h
@@ -3,21 +3,23 @@
 #include "Renderable Objects/RenderableObject.h"
 
 namespace Ruby {
-	class PhongRenderable : public RenderableObject {
-	public:
-		PhongRenderable(std::vector<float>& verticies, std::vector<unsigned int>& indicies, PhongMaterial& mat) 
-			: RenderableObject(verticies, indicies, std::vector<Attribute>{ 3, 3, 2 }), material(std::move(mat)) {
+	namespace Phong {
+		class PhongRenderable : public RenderableObject {
+		public:
+			PhongRenderable(const std::vector<float>& verticies, const std::vector<unsigned int>& indicies, PhongMaterial& mat)
+				: RenderableObject(verticies, indicies, std::vector<Attribute>{ 3, 3, 2 }), material(std::move(mat)) {
 
-		}
+			}
 
-		void render() const override {
-			VAO.bind();
-			ShaderProgram::upload("model", model);
-			ShaderProgram::upload("material", 0, material);
-			glDrawElements(GL_TRIANGLES, numberOfIndicies, GL_UNSIGNED_INT, 0);
-		}
+			void render() const override {
+				VAO.bind();
+				ShaderProgram::upload("model", model);
+				ShaderProgram::upload("material", 0, material);
+				glDrawElements(GL_TRIANGLES, numberOfIndicies, GL_UNSIGNED_INT, 0);
+			}
 
-		Malachite::Matrix4f model{ 1.0f };
-		PhongMaterial material{ };
-	};
+			Malachite::Matrix4f model{ 1.0f };
+			PhongMaterial material{ };
+		};
+	}
 }
