13d7bb35c6ba92b630c2498159d53a78a9b089fc
Ben-Bingham <ben@benbingham.ca>
2022-09-22 16:14:21 -0600

    Moved phong renderalble class

--- Ruby/src/Renderable Objects/Phong/PhongRenderable.h
+++ Ruby/src/Renderable Objects/Phong/PhongRenderable.h
@@ -5,8 +5,8 @@
 namespace Ruby {
 	class PhongRenderable : public RenderableObject {
 	public:
-		PhongRenderable(std::vector<float>& verticies, std::vector<unsigned int>& indicies) 
-			: RenderableObject(verticies, indicies, std::vector<Attribute>{ 3, 3, 2 }) {
+		PhongRenderable(std::vector<float>& verticies, std::vector<unsigned int>& indicies, PhongMaterial& mat) 
+			: RenderableObject(verticies, indicies, std::vector<Attribute>{ 3, 3, 2 }), material(std::move(mat)) {
 
 		}
 
