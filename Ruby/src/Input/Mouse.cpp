17a4fdf2b61feccbcc9be2dfaa5dec21ed96bea6
Ben-Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2022-09-23 09:36:14 -0600

    Created Mouse class

---
+++ Ruby/src/Input/Mouse.cpp
@@ -0,0 +1,54 @@
+#include "Mouse.h"
+#include "IOManager.h"
+
+namespace Ruby {
+	//void Mouse::StartUp() {
+	//	gWindow.setMouseButtonCallback(mouseButtonCallback);
+	//	gWindow.setMousePositionCallback(mousePositionCallback);
+	//	gWindow.setScrollWheelCallback(mouseScrolWheelCallback);
+	//	gWindow.setCursorEntersCallback(cursorEnterCallback);
+	//}
+
+	void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+		IOManager* manager = (IOManager*)glfwGetWindowUserPointer(window);
+
+		switch (button) {
+		case GLFW_MOUSE_BUTTON_1: manager->mouse.button1 = intToMouseState(action); break;
+		case GLFW_MOUSE_BUTTON_2: manager->mouse.button2 = intToMouseState(action); break;
+		case GLFW_MOUSE_BUTTON_3: manager->mouse.button3 = intToMouseState(action); break;
+		}
+	}
+
+	void mousePositionCallback(GLFWwindow* window, double xpos, double ypos) {
+		IOManager* manager = (IOManager*)glfwGetWindowUserPointer(window);
+
+		manager->mouse.yPosition = (int)ypos;
+		manager->mouse.xPosition = (int)xpos;
+
+		int count{ 0 };
+		for (void (*callback)(int xpos, int ypos, void* data) : manager->mouse.mousePositionCallbacks) {
+			callback((int)xpos, (int)ypos, manager->mouse.mousePositionCallbackData[count]);
+			count++;
+		}
+	}
+
+	void mouseScrolWheelCallback(GLFWwindow* window, double xoffset, double yoffset) {
+		IOManager* manager = (IOManager*)glfwGetWindowUserPointer(window);
+
+		int count{ 0 };
+		for (void (*callback)(int xoffset, int yoffset, void* data) : manager->mouse.scrollCallbacks) {
+			callback((int)xoffset, (int)yoffset, manager->mouse.scrollCallbacksData[count]);
+			count++;
+		}
+	}
+
+	void cursorEnterCallback(GLFWwindow* window, int entered) {}
+
+	MouseButtonState intToMouseState(int state) {
+		switch (state) {
+		default:
+		case GLFW_PRESS: return  MouseButtonState::BUTTON_PRESSED;
+		case GLFW_RELEASE: return  MouseButtonState::BUTTON_RELEASED;
+		}
+	}
+}
---
+++ Ruby/src/Input/Mouse.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+
+namespace Ruby {
+	enum MouseButtonState {
+		BUTTON_RELEASED = 0,
+		BUTTON_PRESSED = 1
+	};
+
+	void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+	void mousePositionCallback(GLFWwindow* window, double xpos, double ypos);
+	void mouseScrolWheelCallback(GLFWwindow* window, double xoffset, double yoffset);
+	void cursorEnterCallback(GLFWwindow* window, int entered);
+
+	class Mouse {
+	public:
+		Mouse() { }
+
+		int xPosition{ 0 };
+		int yPosition{ 0 };
+
+		MouseButtonState button1{ MouseButtonState::BUTTON_RELEASED }; // Left
+		MouseButtonState button2{ MouseButtonState::BUTTON_RELEASED }; // Right
+		MouseButtonState button3{ MouseButtonState::BUTTON_RELEASED }; // Middle
+
+		bool hasMoved{ false };
+
+		void addScrollCallback(void (*callback)(int xoffset, int yoffset, void* data), void* data) {
+			scrollCallbacks.push_back(callback);
+			scrollCallbacksData.push_back(data);
+		}
+
+		void addMousePositionCallback(void (*callback)(int xpos, int ypos, void* data), void* data) {
+			mousePositionCallbacks.push_back(callback);
+			mousePositionCallbackData.push_back(data);
+		}
+
+		std::vector<void (*)(int xoffset, int yoffset, void* data)> scrollCallbacks;
+		std::vector<void*> scrollCallbacksData;
+
+		std::vector<void (*)(int xpos, int ypos, void* data)> mousePositionCallbacks;
+		std::vector<void*> mousePositionCallbackData;
+	};
+
+	MouseButtonState intToMouseState(int state);
+}
