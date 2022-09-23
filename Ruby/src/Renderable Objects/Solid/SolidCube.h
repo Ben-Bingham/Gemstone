3987a6a6f11d48a305bb15f373bd6741d84a61b5
Ben-Bingham <ben@benbingham.ca>
2022-09-22 18:34:33 -0600

    Created Solid Renderable Cube class

---
+++ Ruby/src/Renderable Objects/Solid/SolidCube.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "SolidRenderable.h"
+
+namespace Ruby {
+	class SolidCube : public SolidRenderable {
+	public:
+		SolidCube(SolidMaterial& mat)
+			: SolidRenderable(verticies, indices, mat) {
+
+		}
+
+		virtual void render() const {
+			VAO.bind();
+			ShaderProgram::upload("objectColour", material.colour);
+			glDrawElements(GL_TRIANGLES, numberOfIndicies, GL_UNSIGNED_INT, 0);
+		}
+
+	private:
+		static const std::vector<float> verticies;
+		static const std::vector<unsigned int> indices;
+	};
+	
+}
