ad2c2ec2fbe0902e604de479797cfdde6358de7f
Ben-Bingham <ben@benbingham.ca>
2022-09-22 18:34:10 -0600

    Modified PhongRenderable

--- Ruby/src/Renderable Objects/Phong/PhongRenderable.h
+++ Ruby/src/Renderable Objects/Phong/PhongRenderable.h
@@ -1,25 +1,24 @@
 #pragma once
 
-#include "Renderable Objects/RenderableObject.h"
+#include "Renderable Objects/Renderable.h"
 
 namespace Ruby {
-	namespace Phong {
-		class PhongRenderable : public RenderableObject {
-		public:
-			PhongRenderable(const std::vector<float>& verticies, const std::vector<unsigned int>& indicies, PhongMaterial& mat)
-				: RenderableObject(verticies, indicies, std::vector<Attribute>{ 3, 3, 2 }), material(std::move(mat)) {
+	class PhongRenderable : public Renderable {
+	public:
+		PhongRenderable(const std::vector<float>& verticies, const std::vector<unsigned int>& indicies, PhongMaterial& mat)
+			: Renderable(verticies, indicies, std::vector<Attribute>{ 3, 3, 2 }), material(std::move(mat)) {
 
-			}
+		}
 
-			void render() const override {
-				VAO.bind();
-				ShaderProgram::upload("model", model);
-				ShaderProgram::upload("material", 0, material);
-				glDrawElements(GL_TRIANGLES, numberOfIndicies, GL_UNSIGNED_INT, 0);
-			}
+		void render() const override {
+			VAO.bind();
+			ShaderProgram::upload("model", model);
+			ShaderProgram::upload("material", 0, material);
+			glDrawElements(GL_TRIANGLES, numberOfIndicies, GL_UNSIGNED_INT, 0);
+		}
 
-			Malachite::Matrix4f model{ 1.0f };
-			PhongMaterial material{ };
-		};
-	}
+		Malachite::Matrix4f model{ 1.0f };
+		PhongMaterial material{ };
+	};
+	
 }
