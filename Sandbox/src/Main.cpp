51b38b18b56da5bcafafa09e8a414783a05fe6cf
Ben-Bingham <94578530+Ben-Bingham@users.noreply.github.com>
2022-09-23 09:37:00 -0600

    Redid input

--- Sandbox/src/Main.cpp
+++ Sandbox/src/Main.cpp
@@ -10,113 +10,45 @@
 #include "Renderable Objects/Solid/SolidCube.h"
 
 Ruby::Camera camera{ };
-bool firstMouse = true;
-int lastX = 0;
-int lastY = 0;
-float mouseSensitivity = 0.1f;
-Malachite::Degree yaw = -90.0f;
-Malachite::Degree pitch;
+struct FPSController {
+	bool firstMouse = true;
+	int lastX = 0;
+	int lastY = 0;
+	float mouseSensitivity = 0.1f;
+	Malachite::Degree yaw = -90.0f;
+	Malachite::Degree pitch;
+}; 
+FPSController fpsController;
 
-bool keyW;
-bool keyA;
-bool keyS;
-bool keyD;
-bool keySpace;
-bool keyShift;
-bool keyEscape;
+void mousePositionCallback(int xpos, int ypos, void* data) {
+	FPSController* controller = (FPSController*)data;
 
-void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-	if (key == GLFW_KEY_W) {
-		if (action == GLFW_PRESS) {
-			keyW = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keyW = false;
-		}
+	if (controller->firstMouse) {
+		controller->lastX = (int)xpos;
+		controller->lastY = (int)ypos;
+		controller->firstMouse = false;
 	}
 
-	if (key == GLFW_KEY_A) {
-		if (action == GLFW_PRESS) {
-			keyA = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keyA = false;
-		}
-	}
+	float xOffset = (float)(xpos - controller->lastX);
+	float yOffset = (float)(controller->lastY - ypos);
+	controller->lastX = (int)xpos;
+	controller->lastY = (int)ypos;
 
-	if (key == GLFW_KEY_S) {
-		if (action == GLFW_PRESS) {
-			keyS = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keyS = false;
-		}
-	}
+	xOffset *= controller->mouseSensitivity;
+	yOffset *= controller->mouseSensitivity;
 
-	if (key == GLFW_KEY_D) {
-		if (action == GLFW_PRESS) {
-			keyD = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keyD = false;
-		}
-	}
-	
-	if (key == GLFW_KEY_SPACE) {
-		if (action == GLFW_PRESS) {
-			keySpace = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keySpace = false;
-		}
-	}
+	controller->yaw += xOffset;
+	controller->pitch += yOffset;
 
-	if (key == GLFW_KEY_LEFT_SHIFT) {
-		if (action == GLFW_PRESS) {
-			keyShift = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keyShift = false;
-		}
-	}
-
-	if (key == GLFW_KEY_ESCAPE) {
-		if (action == GLFW_PRESS) {
-			keyEscape = true;
-		}
-		else if (action == GLFW_RELEASE) {
-			keyEscape = false;
-		}
-	}
-}
-
-void mousePositionCallback(GLFWwindow* window, double xpos, double ypos) {
-	if (firstMouse) {
-		lastX = (int)xpos;
-		lastY = (int)ypos;
-		firstMouse = false;
-	}
-
-	float xOffset = (float)(xpos - lastX);
-	float yOffset = (float)(lastY - ypos);
-	lastX = (int)xpos;
-	lastY = (int)ypos;
-
-	xOffset *= mouseSensitivity;
-	yOffset *= mouseSensitivity;
-
-	yaw += xOffset;
-	pitch += yOffset;
-
-	if (pitch > 89.0f)
-		pitch = 89.0f;
-	if (pitch < -89.0f)
-		pitch = -89.0f;
+	if (controller->pitch > 89.0f)
+		controller->pitch = 89.0f;
+	if (controller->pitch < -89.0f)
+		controller->pitch = -89.0f;
 
 	Malachite::Vector3f direction;
-	direction.x = cos(Malachite::degreesToRadians(yaw)) * cos(Malachite::degreesToRadians(pitch));
-	direction.y = sin(Malachite::degreesToRadians(pitch));
-	direction.z = sin(Malachite::degreesToRadians(yaw)) * cos(Malachite::degreesToRadians(pitch));
+	direction.x = cos(Malachite::degreesToRadians(controller->yaw)) * cos(Malachite::degreesToRadians(controller->pitch));
+	direction.y = sin(Malachite::degreesToRadians(controller->pitch));
+	direction.z = sin(Malachite::degreesToRadians(controller->yaw)) * cos(Malachite::degreesToRadians(controller->pitch));
 
 	camera.front = Malachite::normalize(direction);
 	camera.updateCameraVectors();
@@ -124,8 +56,10 @@ void mousePositionCallback(GLFWwindow* window, double xpos, double ypos) {
 
 int main() {
 	Ruby::Window window{ };
-	window.setKeyCallback(keyCallback);
-	window.setMousePositionCallback(mousePositionCallback);
+	Ruby::Mouse* mouse = &window.ioManger.mouse;
+	mouse->addMousePositionCallback(mousePositionCallback, (void*)&fpsController);
+
+	Ruby::Keyboard* keyboard = &window.ioManger.keyboard;
 	window.disableCursor();
 
 	Ruby::Renderer renderer{};
@@ -164,31 +98,31 @@ int main() {
 		window.pollEvents();
 		float velocity = 0.05f;
 		 
-		if (keyW) {
+		if (keyboard->KEY_W) {
 			camera.position += camera.front * velocity;
 		}
 
-		if (keyA) {
+		if (keyboard->KEY_A) {
 			camera.position -= camera.right * velocity;
 		}
 
-		if (keyS) {
+		if (keyboard->KEY_S) {
 			camera.position -= camera.front * velocity;
 		}
 
-		if (keyD) {
+		if (keyboard->KEY_D) {
 			camera.position += camera.right * velocity;
 		}
 
-		if (keySpace) {
+		if (keyboard->KEY_SPACE) {
 			camera.position += Malachite::Vector3f{ 0.0f, 1.0f, 0.0f } * velocity;
 		}
 
-		if (keyShift) {
+		if (keyboard->KEY_LEFT_SHIFT) {
 			camera.position -= Malachite::Vector3f{ 0.0f, 1.0f, 0.0f } * velocity;
 		}
 
-		if (keyEscape) {
+		if (keyboard->KEY_ESCAPE) {
 			window.close();
 		}
 
