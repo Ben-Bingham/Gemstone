a2cb1a8afdef35bb3acc0fc1a04e7359195ae285
Ben-Bingham <ben@benbingham.ca>
2022-09-25 15:10:27 -0600

    Created an Ortho function for making orthographic projection matricies

--- Malachite/src/Matrix/MatrixTransformations.h
+++ Malachite/src/Matrix/MatrixTransformations.h
@@ -3,9 +3,9 @@
 
 namespace Malachite {
 	template<typename T>
-	Matrix4x4<T> lookAt(const Vector3<T>& eye, const Vector3<T>& target, const Vector3<T>& up) { //TODO testing
+	Matrix4x4<T> lookAt(const Vector3<T>& eye, const Vector3<T>& target, const Vector3<T>& up) {
 		Vector3<T> direction(normalize(target - eye));
-		Vector3<T> s(normalize(cross(direction, up))); // side??
+		Vector3<T> s(normalize(cross(direction, up)));
 		Vector3<T> relativeUp(cross(s, direction));
 
 		Matrix4x4<T> result{ 1 };
@@ -21,11 +21,11 @@ namespace Malachite {
 		result.row4.x = -dot(s, eye);
 		result.row4.y = -dot(relativeUp, eye);
 		result.row4.z = dot(direction, eye);
-		return result; //Might not work returning local variable
+		return result;
 	}
 
 	template<typename T>
-	Matrix4x4<T> perspective(T fov, T aspectRatio, T zNear, T zFar) { //TODO testing
+	Matrix4x4<T> perspective(T fov, T aspectRatio, T zNear, T zFar) {
 		T tanHalfFov = (T)tan(fov / 2);
 
 		Matrix4x4<T> result{ };
@@ -34,6 +34,18 @@ namespace Malachite {
 		result.row3.z = -(zFar + zNear) / (zFar - zNear);
 		result.row3.w = -1;
 		result.row4.z = -(2 * zFar * zNear) / (zFar - zNear);
-		return result; //Might not work returning local variable
+		return result;
+	}
+
+	template<typename T>
+	Matrix4x4<T> ortho(T left, T right, T bottom, T top, T zNear, T zFar) {
+		Matrix4x4<T> result{ 1.0f };
+		result.row1.x = 2 / (right - left);
+		result.row2.y = 2 / (top - bottom);
+		result.row3.z = -2 / (zFar - zNear);
+		result.row4.x = -(right + left) / (right - left);
+		result.row4.y = -(top + bottom) / (top - bottom);
+		result.row4.z = -(zFar + zNear) / (zFar - zNear);
+		return result;
 	}
 }
