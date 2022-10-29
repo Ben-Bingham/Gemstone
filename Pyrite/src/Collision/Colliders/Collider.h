e0776db37bd6cfb39aa8ac8022bf19683258122b
Ben-Bingham <ben@benbingham.ca>
2022-10-29 17:59:19 -0600

    fixed compiler warning

--- Pyrite/src/Collision/Colliders/Collider.h
+++ Pyrite/src/Collision/Colliders/Collider.h
@@ -15,7 +15,7 @@ namespace Pyrite {
 			Point3D localB{ 0.0_m }; // Furthest point of B into A
 			Direction normal{ 0.0_m }; // localA - localB normalized	// TODO consider removing
 			Meter depth{ 0.0_m }; // Length of localA - localB			// TODO consider removing
-			bool hasCollision;
+			bool hasCollision{ false };
 		};
 
 		virtual Collision collidesWithAABB(const AxisAlignedBoxCollider* box) const = 0;
