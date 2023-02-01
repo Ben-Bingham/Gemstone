e05aca4fd7b07634f7e56ec1bcfb06bc0073e758
Ben-Bingham <ben@benbingham.ca>
2023-01-31 20:05:20 -0700

    cont

--- Ruby/src/Camera.h
+++ Ruby/src/Camera.h
@@ -15,42 +15,13 @@ namespace Ruby {
 	class Camera {
 	public:
 		Camera(Malachite::Vector3f pos = Malachite::Vector3f(0.0f, 0.0f, 0.0f), float fov = 45.0f)
-			:
-		//front(Malachite::Vector3f(0.0f, 0.0f, -1.0f)),
-		m_FOV(fov)
-		//, position(pos) 
-			{
-
-			// updateCameraVectors();
-		}
-
-		// Malachite::Matrix4f& getViewMatrix() {
-		// 	calculateViewMatrix();
-		// 	// return m_ViewMatrix;
-		// }
-
-		// void updateCameraVectors() {
-		// 	right = Malachite::cross(front, Malachite::Vector3f{ 0.0f, 1.0f, 0.0f }).normalize();
-		// 	up = Malachite::cross(right, front).normalize();
-		// }
-		//
-		// void calculateViewMatrix() {
-		// 	viewMatrix = Malachite::lookAt(position, position + front, up);
-		// }
+			: m_FOV(fov) { }
 
 		Malachite::Vector3f forward{ Malachite::Vector3f::north };
-		// Malachite::Vector3f up;
-		// Malachite::Vector3f right;
-
-		// Malachite::Vector3f position;
-
 		RenderingTarget target{ NONE };
-
 		Malachite::Matrix4f viewMatrix{ 1.0f };
 
 	private:
 		float m_FOV;
-
-		// Malachite::Matrix4f m_ViewMatrix;
 	};
 }
