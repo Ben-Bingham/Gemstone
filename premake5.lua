ad0accbdaf208b60a01ec764f92c90ac162ad216
Ben-Bingham <ben@benbingham.ca>
2023-12-12 14:18:35 -0700

    Implemented new project structure

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,56 @@
-workspace "Gemstone"
-	configurations { "Debug", "Release" }
+project "Gemstone"
+    kind "SharedLib"
+    language "C++"
+    cppdialect "C++20"
 
-	-- startproject "Sandbox"
+	flags "MultiProcessorCompile"
 
-	-- include "Sandbox"
-	include "Gemstone"
-	
-	group "Vendor"
-		include "vendor/ImGui"
-	group ""
+    pchheader "include/pch.h"
+    pchsource "src/pch.cpp"
+
+	targetdir "&{wks.location}/build/bin/%{cfg.name}/%{prj.name}"
+	objdir "&{wks.location}/build/obj/%{cfg.name}/%{prj.name}"
+
+    defines { 
+        "GLEW_STATIC",
+        "GEM_ASSETS_STRING=\"assets\\\"",
+        "GLFW_INCLUDE_NONE"
+    }
+
+    filter "configurations:Debug"
+		defines "GEM_DEBUG"
+        runtime "Debug"
+        symbols "On"
+	filter "configurations:Release"
+		defines "GEM_RELEASE"
+        runtime "Release"
+        optimize "On"
+	filter {}
+
+    includedirs {
+        "include",
+        "../dependencies/GLEW/include",
+        "../dependencies/GLFW/include",
+        "../dependencies/ImGui/ImGuiSrc",
+        "../dependencies/ImGui/ImPlotSrc",
+        "../dependencies/stb_image"
+    }
+
+    include "../dependencies/ImGui"
+
+    files {
+        "src/**.cpp",
+        "include/**.h"
+    }
+
+    links {
+        "glew32s",
+        "opengl32",
+        "glfw3",
+        "ImGui"
+    }
+
+    libdirs {
+        "../dependencies/GLFW/lib-vc2022",
+        "../dependencies/GLEW/lib/Release/x64"
+    }
