b7ddb841a90b6c94d019d6801c5acf79de654581
Ben-Bingham <ben@benbingham.ca>
2023-12-28 16:58:43 -0700

    Added TODO

--- include/Rendering/Meshes/MeshObject.h
+++ include/Rendering/Meshes/MeshObject.h
@@ -5,7 +5,7 @@
 namespace Gem {
 	// Contains openGl specific information for meshes, and a hash for that mesh
 	struct MeshObject {
-		MeshObject(const RawMesh& meshData, MeshHash hash)
+		MeshObject(const RawMesh& meshData, MeshHash hash) // TODO move to cpp file
 			: mesh(meshData), hash(hash) {
 			
 		}
